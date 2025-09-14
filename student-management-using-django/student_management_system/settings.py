64309fc30c1dbd3fdc709338d73ecdfcb759665d
jamilahammed19 <jamilahammed19@gmail.com>
2025-09-14 00:28:25 +0000

    all set. lets show the project

--- student-management-using-django/student_management_system/settings.py
+++ student-management-using-django/student_management_system/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'student_management_system.wsgi.application'
+# WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
 
 # Database
@@ -168,3 +168,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost:8000",
+    "http://",
+    "https://",
+]
